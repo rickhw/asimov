4285ab2d2f0991d6438081b6bd84616011a2d1ff
Rick Hwang <rick_kyhwang@hotmail.com>
2025-02-08 09:39:37 +0800

    update on 20250208-0939, comment:

---
+++ push.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source ./scripts/build-config.sh
+
+## TODO: for ARM base
+# mvn spring-boot:build-image \
+#     -Dspring-boot.build-image.imageName=${DOCKER_IMAGE_NAME} \
+#     -Dspring-boot.build-image.additional-args="--platform=linux/arm64" \
+#     -DBUILD_TYPE=dev \
+#     -DHASH_CODE=${HASH_CODE} \
+#     -DBUILD_ID=${BUILD_ID}
+
+mvn spring-boot:build-image \
+    -Dspring-boot.build-image.imageName=${DOCKER_IMAGE_NAME} \
+    -DBUILD_TYPE=${BUILD_TYPE} \
+    -DHASH_CODE=${HASH_CODE} \
+    -DBUILD_ID=${BUILD_ID}
+
+
+./login-ecr.sh
+
+docker push ${DOCKER_IMAGE_NAME}
+
+
+echo "Image Name: $DOCKER_IMAGE_NAME"
+
+# 475976321288.dkr.ecr.us-west-2.amazonaws.com/member-service:web-api_0.3.0-20230909-1105
+#docker tag f57e9af54947ae6c90153fbf31c6d5a5471fcea94b4c78dc6ac76ef2b201eed0 475976321288.dkr.ecr.us-west-2.amazonaws.com/rws-dev:rws.booter_v0.2.0-dev-b20231217-0827
