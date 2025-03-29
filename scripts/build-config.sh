1bca3bc97b788f230fceb40d3e91ba7de32c25b9
Rick Hwang <rick_kyhwang@hotmail.com>
2025-03-29 21:12:50 +0800

    update on 20250329-2112, comment:

--- scripts/build-config.sh
+++ scripts/build-config.sh
@@ -2,11 +2,12 @@
 
 ## docker register
 REPOS_ENDPOINT="475976321288.dkr.ecr.us-west-2.amazonaws.com"
-REPOS_NAME="asimov"
+
 
 ## product metadata
-PRODUCT_NAME="asimov"
-ROLE_NAME="webapi"
+PRODUCT_NAME="rws"
+CODE_NAME="asimov"
+ROLE_NAME="api-server"
 VERSION="0.1.0"
 BUILD_TYPE="dev"
 
@@ -14,12 +15,16 @@ BUILD_TYPE="dev"
 HASH_CODE=$(git rev-parse HEAD)
 BUILD_ID=$(date +%Y%m%d-%H%M)
 
+
+REPOS_NAME="${CODE_NAME}"
+
+## replace the code name with product names
 if [ "${BUILD_TYPE}" == "rel" ]; then
-    REPOS_NAME="rws"
+    REPOS_NAME="${PRODUCT_NAME}"
 fi
 
 ## Artifact metadata
 ARTIFACT_ENDPOINT="${REPOS_ENDPOINT}/${REPOS_NAME}"
-DOCKER_IMAGE_NAME="${ARTIFACT_ENDPOINT}:${PRODUCT_NAME}.${ROLE_NAME}_v${VERSION}-${BUILD_TYPE}-b${BUILD_ID}"
-DOCKER_IMAGE_LATEST_NAME="${ARTIFACT_ENDPOINT}:${PRODUCT_NAME}.${ROLE_NAME}_v${VERSION}-${BUILD_TYPE}-latest"
+DOCKER_IMAGE_NAME="${ARTIFACT_ENDPOINT}:${ROLE_NAME}_v${VERSION}-${BUILD_TYPE}-b${BUILD_ID}"
+DOCKER_IMAGE_LATEST_NAME="${ARTIFACT_ENDPOINT}:${ROLE_NAME}_v${VERSION}-${BUILD_TYPE}-latest"
 
