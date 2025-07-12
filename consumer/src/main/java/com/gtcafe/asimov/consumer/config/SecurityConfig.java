d7a77678b9df70c23199c3fd6d6f18788ec2fafc
Rick Hwang <rick_kyhwang@hotmail.com>
2025-07-12 12:13:00 +0800

    update on 20250712-1213, comment:

--- consumer/src/main/java/com/gtcafe/asimov/consumer/config/SecurityConfig.java
+++ consumer/src/main/java/com/gtcafe/asimov/consumer/config/SecurityConfig.java
@@ -1,37 +1,37 @@
-// package com.gtcafe.asimov.consumer.config;
+package com.gtcafe.asimov.consumer.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.http.HttpMethod;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-// import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.Slf4j;
 
-// @Configuration
-// @Slf4j
-// @EnableWebSecurity
-// public class SecurityConfig {
+@Configuration
+@Slf4j
+@EnableWebSecurity
+public class SecurityConfig {
 
-//     @Bean
-//     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//         return httpSecurity
-//             .authorizeHttpRequests(requests -> requests
-//                 .requestMatchers("/css/**", "/js/**", "/images/**").permitAll() // 允許訪問靜態資源
-//                 // .requestMatchers(HttpMethod.POST, "/api/members").permitAll()
-//                 // .requestMatchers("/api/v1alpha/tokens").permitAll()
-//                 // .requestMatchers("/api/v1alpha/users").permitAll()
-//                 .requestMatchers("/api/**").permitAll()
-//                 .requestMatchers("/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
-//                 .requestMatchers("/actuator/**").permitAll()
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/css/**", "/js/**", "/images/**").permitAll() // 允許訪問靜態資源
+                // .requestMatchers(HttpMethod.POST, "/api/members").permitAll()
+                // .requestMatchers("/api/v1alpha/tokens").permitAll()
+                // .requestMatchers("/api/v1alpha/users").permitAll()
+                .requestMatchers("/api/**").permitAll()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
+                .requestMatchers("/actuator/prometheus").permitAll()
 
-//                 .requestMatchers(HttpMethod.GET, "/_/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/_/**").permitAll()
                 
-//                 .anyRequest().authenticated()
-//             )
+                .anyRequest().authenticated()
+            )
 
-//             .build();
-//     }
+            .build();
+    }
    
-// }
+}
