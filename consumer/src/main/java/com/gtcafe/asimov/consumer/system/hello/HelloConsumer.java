443f874973f829a8c40ad09e85a4a91bfc7856e8
Rick Hwang <rick_kyhwang@hotmail.com>
2025-07-17 05:47:45 +0800

    update on 20250717-0547, comment:

--- consumer/src/main/java/com/gtcafe/asimov/consumer/system/hello/HelloConsumer.java
+++ consumer/src/main/java/com/gtcafe/asimov/consumer/system/hello/HelloConsumer.java
@@ -1,22 +1,23 @@
 package com.gtcafe.asimov.consumer.system.hello;
 
-import com.gtcafe.asimov.consumer.system.hello.config.HelloConsumerAsyncConfig;
-import com.gtcafe.asimov.framework.utils.JsonUtils;
-import com.gtcafe.asimov.system.hello.model.HelloEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Optional;
+
+import org.slf4j.log;
+import org.slf4j.logFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-import java.util.Optional;
+import com.gtcafe.asimov.consumer.system.hello.config.HelloConsumerAsyncConfig;
+import com.gtcafe.asimov.framework.utils.JsonUtils;
+import com.gtcafe.asimov.system.hello.model.HelloEvent;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class HelloConsumer {
-
-    private static final Logger logger = LoggerFactory.getLogger(HelloConsumer.class);
 
     @Autowired
     private JsonUtils jsonUtils;
@@ -26,12 +27,12 @@ public class HelloConsumer {
 
     @RabbitListener(queues = HelloConsumerAsyncConfig.QUEUE_NAME)
     public void receiveMessage(@Payload String eventString) {
-        logger.info("Received message on main queue: {}", eventString);
+        log.info("Received message on main queue: {}", eventString);
 
         Optional<HelloEvent> eventOptional = jsonUtils.jsonStringToModelSafe(eventString, HelloEvent.class);
 
         if (eventOptional.isEmpty()) {
-            logger.error("Failed to deserialize message to HelloEvent: {}", eventString);
+            log.error("Failed to deserialize message to HelloEvent: {}", eventString);
             // If deserialization fails, we treat it as a permanent failure and send to DLQ
             throw new RuntimeException("Deserialization failed for message: " + eventString);
         }
@@ -45,9 +46,9 @@ public class HelloConsumer {
                 // We re-throw to trigger the DLQ mechanism
                 throw new RuntimeException("Business logic failed for event: " + event.getId());
             }
-            logger.info("Processed event successfully: {}", event.getId());
+            log.info("Processed event successfully: {}", event.getId());
         } catch (Exception e) {
-            logger.error("Error processing event: {}", event.getId(), e);
+            log.error("Error processing event: {}", event.getId(), e);
             // Re-throw the exception to trigger the DLQ mechanism
             throw e;
         }
@@ -55,7 +56,7 @@ public class HelloConsumer {
 
     @RabbitListener(queues = HelloConsumerAsyncConfig.QUEUE_NAME + HelloConsumerAsyncConfig.DLQ_SUFFIX)
     public void receiveDeadLetterMessage(@Payload String eventString) {
-        logger.error("Received dead-lettered message: {}. Storing for analysis.", eventString);
+        log.error("Received dead-lettered message: {}. Storing for analysis.", eventString);
         // Here you can add logic to handle the failed message,
         // e.g., save it to a database, send a notification, etc.
     }
